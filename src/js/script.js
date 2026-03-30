7b4cf5bbad20342845ec2a41679cef9c9d5b426c
Eduarda <dudamoraes1402@gmail.com>
2026-03-30 09:15:50 -0300

    Feat: Declarações e Variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,49 @@
-alert("Hello Word!")
+//Declarações e Variáveis
+//var,let e const
+
+var nome="Fiap";
+console.log(nome);
+
+var nome="Teste";
+console.log(nome)
+
+if(true){
+    var apelido="Fiapinho";
+    console.log(apelido);
+}
+
+const taxaJuro=0.05;
+console.log(taxaJuro);
+//taxaJuros=1.5;
+
+//typeof-Verifica o tipo da variavel
+var exemplo="ola-dev-let";
+console.log(exemplo);
+let exemplo1="ola-dev-let";
+console.log(exemplo1)
+const exemplo2="ola-de-const";
+console.log(exemplo2);
+
+let exemplo3=10;
+console.log(exemplo3)
+
+let exemplo4=true;
+console.log(exemplo4);
+
+let exemplo5={nome:"teste"}
+console.log(exemplo5)
+
+let exemplo6=["Java", "C#", "PDP"];
+console.log(typeof exemplo6)
+
+//valor undefined
+let exemplo7;
+console.log(exemplo7)
+
+//valor vazio
+let exemplo8=null;
+console.log(exemplo8)
+
+//(Metodo)
+//{Objeto}
+//[Array]
